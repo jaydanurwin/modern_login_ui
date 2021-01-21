458dc25313f24838efc2da8978db3b4a793f72b4
Jaydan Urwin <jaydan@jaydanurwin.com>
2021-01-21 11:37:47 -0700

    removed completed pieces

--- lib/screens/login/sign_in.dart
+++ lib/screens/login/sign_in.dart
@@ -12,10 +12,6 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   // text field state
-  String email = '';
-  String password = '';
-  String error = '';
-  bool _passwordIsHidden = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,106 +19,7 @@ class _SignInState extends State<SignIn> {
       body: SafeArea(
         child: ListView(
           padding: EdgeInsets.symmetric(horizontal: 32.0),
-          children: <Widget>[
-            Container(
-              height: MediaQuery.of(context).size.height * 0.85,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      SizedBox(
-                        height: 50,
-                      ),
-                      Text(
-                        'Login',
-                        style: Theme.of(context)
-                            .textTheme
-                            .headline2
-                            .copyWith(color: Colors.black),
-                      ),
-                      SizedBox(
-                        height: 18,
-                      ),
-                      // Email Text field
-                      TextFormField(
-                        decoration: InputDecoration(labelText: 'Email address'),
-                        validator: (val) =>
-                            val.isEmpty ? 'Enter an email' : null,
-                        onChanged: (val) {
-                          setState(() => email = val);
-                        },
-                      ),
-                      SizedBox(
-                        height: 18.0,
-                      ),
-                      // Password Text Field
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'Password',
-                          suffixIcon: GestureDetector(
-                            child: IconButton(
-                              onPressed: () {
-                                setState(() {
-                                  _passwordIsHidden = !_passwordIsHidden;
-                                });
-                              },
-                              icon: Icon(
-                                _passwordIsHidden
-                                    ? Icons.visibility
-                                    : Icons.visibility_off,
-                              ),
-                            ),
-                          ),
-                        ),
-                        obscureText: _passwordIsHidden,
-                        validator: (val) => val.length < 8
-                            ? 'Enter a password 8+ characters long'
-                            : null,
-                        onChanged: (val) {
-                          setState(() => password = val);
-                        },
-                      ),
-                      SizedBox(
-                        height: 50,
-                      ),
-                      CustomCircleButton(
-                        onPressed: () {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => Home()));
-                        },
-                      ),
-                    ],
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'Need to create an account?',
-                        style: Theme.of(context).textTheme.bodyText1,
-                      ),
-                      SizedBox(
-                        height: 16,
-                      ),
-                      FlatButton(
-                        color: Theme.of(context).primaryColor,
-                        onPressed: () {
-                          widget.toggleLogin();
-                        },
-                        child: Text(
-                          'Sign Up'.toUpperCase(),
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        shape: const StadiumBorder(),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
+          children: <Widget>[],
         ),
       ),
     );
--- lib/screens/login/sign_up.dart
+++ lib/screens/login/sign_up.dart
@@ -12,128 +12,28 @@ class SignUp extends StatefulWidget {
 
 class _SignUpState extends State<SignUp> {
   // text field state
-  String name = '';
-  String email = '';
-  String password = '';
-  String error = '';
-  bool _passwordIsHidden = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
-      body: SafeArea(
-        child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: 32.0),
-          children: <Widget>[
-            Container(
-              height: MediaQuery.of(context).size.height * 0.85,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      SizedBox(
-                        height: 50,
-                      ),
-                      Text(
-                        'Sign Up',
-                        style: Theme.of(context)
-                            .textTheme
-                            .headline2
-                            .copyWith(color: Colors.black),
-                      ),
-                      // Name text field
-                      TextFormField(
-                        decoration: InputDecoration(labelText: 'Name'),
-                        validator: (val) =>
-                            val.isEmpty ? 'Enter your name' : null,
-                        onChanged: (val) {
-                          setState(() => name = val);
-                        },
-                      ),
-                      SizedBox(
-                        height: 18,
-                      ),
-                      // Email Text field
-                      TextFormField(
-                        decoration: InputDecoration(labelText: 'Email address'),
-                        validator: (val) =>
-                            val.isEmpty ? 'Enter an email' : null,
-                        onChanged: (val) {
-                          setState(() => email = val);
-                        },
-                      ),
-                      SizedBox(
-                        height: 18.0,
-                      ),
-                      // Password Text Field
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'Password',
-                          suffixIcon: GestureDetector(
-                            child: IconButton(
-                              onPressed: () {
-                                setState(() {
-                                  _passwordIsHidden = !_passwordIsHidden;
-                                });
-                              },
-                              icon: Icon(
-                                _passwordIsHidden
-                                    ? Icons.visibility
-                                    : Icons.visibility_off,
-                              ),
-                            ),
-                          ),
-                        ),
-                        obscureText: _passwordIsHidden,
-                        validator: (val) => val.length < 8
-                            ? 'Enter a password 8+ characters long'
-                            : null,
-                        onChanged: (val) {
-                          setState(() => password = val);
-                        },
-                      ),
-                      SizedBox(
-                        height: 50,
-                      ),
-                      CustomCircleButton(
-                        onPressed: () {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => Home()));
-                        },
-                      ),
-                    ],
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'Already have an account?',
-                        style: Theme.of(context).textTheme.bodyText1,
-                      ),
-                      SizedBox(
-                        height: 16,
-                      ),
-                      FlatButton(
-                        color: Theme.of(context).primaryColor,
-                        onPressed: () {
-                          widget.toggleLogin();
-                        },
-                        child: Text(
-                          'Login'.toUpperCase(),
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        shape: const StadiumBorder(),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 32.0),
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height * 0.85,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[],
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
